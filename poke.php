bb6be23967fddfa2d301511d223c5de7b18a57d8
Keith Armstrong <keith1015@gmail.com>
2014-02-22 05:31:00 -0600

    Added testing mode and improved UI

--- poke.php
+++ poke.php
@@ -1,55 +1,88 @@
 <?php
 require_once("global.php");
 
-// Poke episode to download
-$id = $argv[1];
+// Attempt to fetch all pokemon episodes
+if ($argv[1] == "test") {
+    for ($a = 1; $a < 808; $a++) {
+        $episode = poke($a);
+        if ($episode != null) {
+            echo "\033[32mFound episode " . $a . " - method #" . $episode[1] . " - " . $episode[0] . "\033[0m\n";
+        } else {
+            echo "\033[31mEpisode " . $a . " not found!\033[0m\n";
+        }
+    }
+// Retrieve a single specified episode
+} else {
+    $id = $argv[1];
 
-$found = false;
-$attempt = 0;
-while (!$found) {
-    // Give up after 3 (can't quite crack how the 4th one works yet) methods
-    if (++$attempt == 4) {
-        die("Failed to locate pokemon episode :(\n");
+    // Error message if no episode specified
+    if ($id == null) {
+        die("\033[31mError: No episode specified!\n\033[0mUsage: php poke.php [episode_number]\n");
     }
 
-    // Try to download via loadup.ru
-    if ($attempt == 1) {
-        $data = file_get_contents("http://pokemonepisode.org/1.php?P-ID=" . $id);
+    // Start timer
+    $time_start = microtime(true);
 
-        // Detect if valid video url is found
-        $val = extractData($data, "file=", "\" wid");
-        if ($val != false) {
-            $found = true;
-            $url = $val;
-        }
-    // Try to download via VideoBam
-    } else if ($attempt == 2) {
-        $pre_data = file_get_contents("http://pokemonepisode.org/2.php?P-ID=" . $id);
-        $data = file_get_contents(extractData($pre_data, "src=\"", "\""));
+    // Fetch episode data
+    echo "Attempting to retrieve Pokemon Episode \033[32m#" . $id . "\033[0m";
 
-        // Detect if valid video url is found
-        $val = extractData($data, "high: '", "'");
-        if ($val != false) {
-            $found = true;
-            $url = $val;
-        }
-    // Try to download via DailyMotion
-    } else if ($attempt == 3) {
-        $pre_data = file_get_contents("http://pokemonepisode.org/3.php?P-ID=" . $id);
-        $data = file_get_contents(extractData($pre_data, "src=\"", "\""));
-
-        // Detect if valid video url is found
-        $val = extractData($data, "stream_h264_url\":\"", "\"");
-        if ($val != false) {
-            $found = true;
-
-            // Since DailyMotion adds extraneous slashes
-            $url = stripslashes($val);
-        }
+    $episode = poke($id);
+    if ($episode != null) {
+        echo "\n\033[32mFound episode " . $id . " - method #" . $episode[1] . " - " . $episode[0] . "\033[0m";
+        echo "\n\nData fetched in " . (microtime(true)-$time_start) . " seconds.\n";
     } else {
-        die;
+        echo "\n\033[31mEpisode " . $id . " not found!\033[0m\n";
     }
 }
-echo "attempt #" . $attempt . "\n";
-echo $url . "\n";
+
+function poke($id = 1) {
+    $found = false;
+    $method = 0;
+
+    while (!$found) {
+        // Give up after 3 (can't quite crack how the 4th one works yet) methods
+        if (++$method == 4) {
+            return null;
+        }
+
+        // Try to download via loadup.ru
+        if ($method == 1) {
+            $data = file_get_contents("http://pokemonepisode.org/1.php?P-ID=" . $id);
+
+            // Detect if valid video url is found
+            $val = extractData($data, "file=", "\" wid");
+            if ($val != false) {
+                $found = true;
+                $url = $val;
+            }
+        // Try to download via VideoBam
+        } else if ($method == 2) {
+            $pre_data = file_get_contents("http://pokemonepisode.org/2.php?P-ID=" . $id);
+            $data = file_get_contents(extractData($pre_data, "src=\"", "\""));
+
+            // Detect if valid video url is found
+            $val = extractData($data, "high: '", "'");
+            if ($val != false) {
+                $found = true;
+                $url = $val;
+            }
+        // Try to download via DailyMotion
+        } else if ($method == 3) {
+            $pre_data = file_get_contents("http://pokemonepisode.org/3.php?P-ID=" . $id);
+            $data = file_get_contents(extractData($pre_data, "src=\"", "\""));
+
+            // Detect if valid video url is found
+            $val = extractData($data, "stream_h264_url\":\"", "\"");
+            if ($val != false) {
+                $found = true;
+
+                // Since DailyMotion adds extraneous slashes
+                $url = stripslashes($val);
+            }
+        } else {
+            return null;
+        }
+    }
+    return array($url, $method);
+}
 ?>
