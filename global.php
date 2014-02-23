0a876ffca6a8510cde9c3d2c8f16c359d794a2b8
Keith Armstrong <keith1015@gmail.com>
2014-02-22 22:14:36 -0600

    Worked on downloading functionality

--- global.php
+++ global.php
@@ -1,4 +1,24 @@
 <?php
+$seanum = array(1 => 1, 83, 119, 160, 212, 277, 317, 369, 423, 469, 521, 573, 626, 660, 710, 759, 784, 804, 808);
+$seaname = array(1 => "Indigo League",
+    "The Orange Island League",
+    "The Johto Journeys",
+    "Johto League Champions",
+    "Master Quest",
+    "Advanced",
+    "Advanced Challenge",
+    "Advanced Battle",
+    "Battle Frontier",
+    "Diamond and Pearl",
+    "Diamond and Pearl Battle Dimension",
+    "Diamond and Pearl Galactic Battle",
+    "Diamond and Pearl Sinnoh League Victors",
+    "Black and White",
+    "Black and White Rival Destinies",
+    "Black and White Adventures in Unova",
+    "Black and White Adventures in Unova and Beyond",
+    "XY");
+
 function extractData($data, $search, $ending, $specific = -1) {
 	$matches = findall($search, $data);
 	foreach ($matches as &$val) {
@@ -49,5 +69,92 @@ function findall($needle, $haystack) {
         return $found;
     }
     return false;
+}
+
+function poke($id = 1, $method = 1) {
+    // To fix the method number the user selects
+    --$method;
+
+    $found = false;
+
+    while (!$found) {
+        // Give up after 3 (can't quite crack how the 4th one works yet) methods
+        if (++$method == 4) {
+            return null;
+        }
+
+        // Try to download via loadup.ru
+        if ($method == 1) {
+            $data = file_get_contents("http://pokemonepisode.org/1.php?P-ID=" . $id);
+
+            // Detect if valid video url is found
+            $val = extractData($data, "file=", "\" wid");
+            if ($val != false) {
+                $found = true;
+                $url = $val;
+            }
+        // Try to download via VideoBam
+        } else if ($method == 2) {
+            $pre_data = file_get_contents("http://pokemonepisode.org/2.php?P-ID=" . $id);
+            $data = file_get_contents(extractData($pre_data, "src=\"", "\""));
+
+            // Detect if valid video url is found
+            $val = extractData($data, "high: '", "'");
+            if ($val != false) {
+                $found = true;
+                $url = $val;
+            }
+        // Try to download via DailyMotion
+        } else if ($method == 3) {
+            $pre_data = file_get_contents("http://pokemonepisode.org/3.php?P-ID=" . $id);
+            $data = file_get_contents(extractData($pre_data, "src=\"", "\""));
+
+            // Detect if valid video url is found
+            $val = extractData($data, "stream_h264_url\":\"", "\"");
+            if ($val != false) {
+                $found = true;
+
+                // Since DailyMotion adds extraneous slashes
+                $url = stripslashes($val);
+            }
+        } else {
+            return null;
+        }
+    }
+    return array($url, $method);
+}
+
+function pokeTitle($num) {
+    $data = file_get_contents("http://pokemonepisode.org/episode-" . $num);
+    $title = extractData($data, "&#8211; ", "<", 1);
+    return $title;
+}
+
+function download($num, $title, $url) {
+    global $seanum, $seaname;
+
+    mkdir("pokemon");
+
+    // File extension
+    $headers = get_headers($url);
+    $ext = $headers[3];
+    if ($ext == "Content-Type: video/x-flv") {
+        $ext = "flv";
+    } else {
+        $ext = "mp4";
+    }
+    mkdir("pokemon/" . seasonNum($num) . " - " . $seaname[seasonNum($num)]);
+    shell_exec("wget -O \"pokemon/" . seasonNum($num) . " - " . $seaname[seasonNum($num)] . "/[" . $num . "] - " . $title . "." . $ext . "\" " . $url . " --continue");
+
+}
+
+function seasonNum($episode) {
+    global $seanum, $seaname;
+    
+    for ($a = 1; $a < count($seanum); $a++) {
+        if ($episode >= $seanum[$a] && $episode < $seanum[($a+1)]) {
+            return $a;
+        }
+    }
 }
 ?>
--- poke.php
+++ poke.php
@@ -4,18 +4,41 @@ require_once("global.php");
 // Argument mode/episode number
 $arg = $argv[1];
 
+/*
+// Specific force method
+$method = $argv[2];
+if ($method == null) {
+    $method = 1;
+} else if (!is_numeric($method) || $method > 3 || $method < 1) {
+    die("Error: Specified method is invalid!\n");
+}
+*/
+$method = 1;
+
+$save = $argv[2];
+
+if ($save == null) {
+    $save = false;
+} else if ($save != "false" && $save != "true") {
+    die("Error: Save mode is either true or false!\n");
+}
+
 // Attempt to fetch all pokemon episodes
-if ($arg == "test") {
+if ($arg == "all") {
 
     // Start timer
     $time_start = microtime(true);
 
     for ($a = 1; $a < 808; $a++) {
-        $episode = poke($a);
+        $episode = poke($a, $method);
         if ($episode != null) {
-            echo "\033[32mFound episode " . $a . " - method #" . $episode[1] . " - " . $episode[0] . "\033[0m\n";
+            $title = pokeTitle($a);
+            echo "Found episode " . $a . " (" . $title . ") - method #" . $episode[1] . " - " . $episode[0] . "\n";
+            if ($save == "true") {
+                download($a, $title, $episode[0]);
+            }
         } else {
-            echo "\033[31mEpisode " . $a . " not found!\033[0m\n";
+            echo "Episode " . $a . " not found!\n";
         }
     }
     echo "\nData fetched in " . (microtime(true)-$time_start) . " seconds.\n";
@@ -29,17 +52,21 @@ if ($arg == "test") {
     $range = explode("-", $arg);
     // Range error checking
     if ($range[0] > $range[1] || !is_numeric($range[0]) || !is_numeric($range[1])) {
-        die("\033[31mError: Episode range is invalid!\n");
+        die("Error: Episode range is invalid!\n");
     }
 
     // Fetch episode data
-    echo "Attempting to retrieve Pokemon Episodes \033[32m" . $range[0] . "\033[0m - \033[32m" . $range[1] . "\033[0m\n";
+    echo "Attempting to retrieve Pokemon Episodes " . $range[0] . " - " . $range[1] . "\n";
     for ($a = $range[0]; $a <= $range[1]; $a++) {
-        $episode = poke($a);
+        $episode = poke($a, $method);
         if ($episode != null) {
-            echo "\033[32mFound episode " . $a . " - method #" . $episode[1] . " - " . $episode[0] . "\033[0m\n";
+            $title = pokeTitle($a);
+            echo "Found episode " . $a . " (" . $title . ") - method #" . $episode[1] . " - " . $episode[0] . "\n";
+            if ($save == "true") {
+                download($a, $title, $episode[0]);
+            }
         } else {
-            echo "\033[31mEpisode " . $a . " not found!\033[0m\n";
+            echo "Episode " . $a . " not found!\n";
         }
     }
     echo "\nData fetched in " . (microtime(true)-$time_start) . " seconds.\n";
@@ -49,74 +76,27 @@ if ($arg == "test") {
 
     // Error message if no episode specified
     if ($arg == null) {
-        die("\033[31mError: No episode specified!\n\033[0mUsage: php poke.php [test | episode_number | range]\n");
+        die("Error: No episode specified!\nUsage: php poke.php [all | episode_number | range]\n");
     }
 
     // Start timer
     $time_start = microtime(true);
 
     // Fetch episode data
-    echo "Attempting to retrieve Pokemon Episode \033[32m#" . $arg . "\033[0m";
-    $episode = poke($arg);
+    echo "Attempting to retrieve Pokemon Episode #" . $arg;
+    $episode = poke($arg, $method);
 
     if ($episode != null) {
-        echo "\n\033[32mFound episode " . $arg . " - method #" . $episode[1] . " - " . $episode[0] . "\033[0m";
+        $title = pokeTitle($arg);
+        echo "\nFound episode " . $arg . " (" . $title . ") - method #" . $episode[1] . " - " . $episode[0];
+        if ($save == "true") {
+            download($arg, $title, $episode[0]);
+        }
     } else {
-        echo "\n\033[31mEpisode " . $arg . " not found!\033[0m\n";
+        echo "\nEpisode " . $arg . " not found!\n";
     }
     echo "\n\nData fetched in " . (microtime(true)-$time_start) . " seconds.\n";
 } else {
-    die("\033[31mError: Unknown input!\n\033[0mUsage: php poke.php [test | episode_number | range]\n");
-}
-
-function poke($id = 1) {
-    $found = false;
-    $method = 0;
-
-    while (!$found) {
-        // Give up after 3 (can't quite crack how the 4th one works yet) methods
-        if (++$method == 4) {
-            return null;
-        }
-
-        // Try to download via loadup.ru
-        if ($method == 1) {
-            $data = file_get_contents("http://pokemonepisode.org/1.php?P-ID=" . $id);
-
-            // Detect if valid video url is found
-            $val = extractData($data, "file=", "\" wid");
-            if ($val != false) {
-                $found = true;
-                $url = $val;
-            }
-        // Try to download via VideoBam
-        } else if ($method == 2) {
-            $pre_data = file_get_contents("http://pokemonepisode.org/2.php?P-ID=" . $id);
-            $data = file_get_contents(extractData($pre_data, "src=\"", "\""));
-
-            // Detect if valid video url is found
-            $val = extractData($data, "high: '", "'");
-            if ($val != false) {
-                $found = true;
-                $url = $val;
-            }
-        // Try to download via DailyMotion
-        } else if ($method == 3) {
-            $pre_data = file_get_contents("http://pokemonepisode.org/3.php?P-ID=" . $id);
-            $data = file_get_contents(extractData($pre_data, "src=\"", "\""));
-
-            // Detect if valid video url is found
-            $val = extractData($data, "stream_h264_url\":\"", "\"");
-            if ($val != false) {
-                $found = true;
-
-                // Since DailyMotion adds extraneous slashes
-                $url = stripslashes($val);
-            }
-        } else {
-            return null;
-        }
-    }
-    return array($url, $method);
+    die("Error: Unknown input!\nUsage: php poke.php [all | episode_number | range]\n");
 }
 ?>
